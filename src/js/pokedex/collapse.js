08ca2eff54c3c0f5bd3a282982b631b47e8af793
luc457x <lucas7eixeir4@outlook.com>
2025-02-20 19:18:39 -0300

    fix ident

--- src/js/pokedex/collapse.js
+++ src/js/pokedex/collapse.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const collapseButton = document.getElementById('collapse-button');
-    const mainContent = document.querySelector('.collapse');
+document.addEventListener('DOMContentLoaded', function () {
+	const collapseButton = document.getElementById('collapse-button');
+	const mainContent = document.querySelector('.collapse');
 
-    console.log('Initial scrollHeight:', mainContent.scrollHeight);
+	console.log('Initial scrollHeight:', mainContent.scrollHeight);
 
-    mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
+	mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
 
-    function updateMaxHeight() {
-        if (mainContent.style.maxHeight !== '1px') {
-            mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
-        }
-    }
+	function updateMaxHeight() {
+		if (mainContent.style.maxHeight !== '1px') {
+			mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
+		}
+	}
 
-    mainContent.offsetHeight;
+	mainContent.offsetHeight;
 
-    window.addEventListener('resize', updateMaxHeight);
+	window.addEventListener('resize', updateMaxHeight);
 
-    collapseButton.addEventListener('click', function() {
-        if (mainContent.style.maxHeight !== '1px') {
-            mainContent.style.maxHeight = '1px';
-            mainContent.style.overflowY = 'hidden';
-        } else {
-            mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
-        }
-    });
+	collapseButton.addEventListener('click', function () {
+		if (mainContent.style.maxHeight !== '1px') {
+			mainContent.style.maxHeight = '1px';
+			mainContent.style.overflowY = 'hidden';
+		} else {
+			mainContent.style.maxHeight = mainContent.scrollHeight + 'px';
+		}
+	});
 });
